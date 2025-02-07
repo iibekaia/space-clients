da8736e24741210df0cbd9eca2468eca9fb8a139
imeda <imeda.bekaia252@ens.tsu.edu.ge>
2025-02-07 16:54:05 +0400

    Added some examples for json-server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, Inject, OnInit, Optional} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,33 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'clients';
+
+  constructor(
+    @Optional()
+    @Inject('CONFIG')
+    public CONFIG: { API_URL: string },
+    private http: HttpClient,
+  ) {
+    if (!this.CONFIG) {
+      this.CONFIG = {API_URL: '/',};
+    }
+  }
+
+  ngOnInit() {
+    // this.addUser({name: 'new imedaaa', email: 'imedaaa@gmail.com'}).subscribe();
+    setTimeout(() => {
+      this.loadUsers();
+    }, 100)
+
+  }
+
+  loadUsers() {
+    this.http.get<any>(`${this.CONFIG.API_URL}/users`).subscribe()
+  }
+
+  addUser(user: { name: string; email: string }): Observable<any> {
+    return this.http.post(`${this.CONFIG.API_URL}/users`, user);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,18 +3,24 @@ import {provideRouter} from '@angular/router';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
 import Aura from '@primeng/themes/aura';
-
 import {routes} from './app.routes';
+import {API_URL} from '../config/config';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
     providePrimeNG({
       theme: {
         preset: Aura
       }
-    })
+    }),
+    {
+      provide: 'CONFIG',
+      useValue: {API_URL},
+    },
   ]
 };
